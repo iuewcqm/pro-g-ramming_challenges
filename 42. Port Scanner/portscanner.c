c42082d8c5c0d51aaf8746dc7e1951a4097e74c5
iuewcqm <capricorn.k@yandex.ru>
2023-10-03 20:49:31 +0300

    some changes

--- 42. Port Scanner/portscanner.c
+++ 42. Port Scanner/portscanner.c
@@ -7,40 +7,43 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define PORT_LEN 6
-
-#define BUF_SIZE 500
+#define PORT_LENGTH
 
 void usage(char **argv) {
-  printf("Usage:\n   %s <IP> <PORT>\n", argv[0]);
+  printf("Usage:\n   %s <IP> [PORT]\n", argv[0]);
 }
 
 struct addrinfo create_default_addr_hints() {
-  struct addrinfo *hints = (struct addrinfo *)malloc(sizeof(struct addrinfo));
-  hints->ai_family   = AF_INET;
-  hints->ai_socktype = SOCK_STREAM;
-  hints->ai_protocol = IPPROTO_TCP;
-  hints->ai_flags    = 0;
-  return *hints;
+  struct addrinfo hints = { 0 };
+  hints.ai_family   = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_protocol = IPPROTO_TCP;
+  hints.ai_flags    = 0;
+  return hints;
 }
 
-void scan_port(const char *ip_addr, const char *port) {
+int is_open_port(const char *ip_addr, const char *port) {
   struct addrinfo hints = create_default_addr_hints();
-  struct addrinfo *host_info = (struct addrinfo *)malloc(sizeof(struct addrinfo));
+  struct addrinfo *host_info;
   int result = getaddrinfo(ip_addr, port, &hints, &host_info);
   if (result != 0) {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(result));
     exit(EXIT_FAILURE);
   }
 
-  int sockfd = socket(host_info->ai_family, host_info->ai_socktype, host_info->ai_protocol);
+  int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   if (sockfd != -1) {
     int result = connect(sockfd, host_info->ai_addr, host_info->ai_addrlen);
-    if (result != -1)
-      printf("port %s open\n", port);
-    else
-      printf("port %s closed\n", port);
+    if (result != -1) {
+      close(sockfd);
+      return 1;
+    }
+    else {
+      close(sockfd);
+      return 0;
+    }
   }
+  return -1;
 }
 
 char *resolve_host(const char *__remote_host) {
@@ -54,14 +57,30 @@ char *resolve_host(const char *__remote_host) {
 }
 
 int main(int argc, char **argv) {
-  if (argc < 3) {
+  if (argc == 2) {
+    char *remote_ip = resolve_host(argv[1]);
+    for(int i = 1; i < 65536; i++) {
+      char port[5];
+      snprintf(port, 6, "%d", i);
+      if (is_open_port(remote_ip, port) == 1) {
+	printf("%s/tcp open\n", port);
+      }
+    }
+  }
+  else if (argc == 3) {
+    char *remote_ip = resolve_host(argv[1]);
+    char *port = argv[2];
+    if (is_open_port(remote_ip, port) == 1) {
+      printf("%s/tcp open\n", port);
+    }
+    else {
+      printf("%s/tcp closed\n", port);
+    }
+  }
+  else {
     usage(argv);
     exit(EXIT_FAILURE);
   }
-  
-  char *remote_ip = resolve_host(argv[1]);
-  char *port      = argv[2];
-  scan_port(remote_ip, port);
   
   return 0;
 }
